dd47753382a5de570ed69cf17ce23222f1f0b1ef
adlermo <adlerorteney@gmail.com>
2022-07-02 00:34:16 -0300

    Added icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 
 import './App.css';
 
@@ -7,12 +6,12 @@ import LoginGoogle from './components/LoginGoogle';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <h1 className="text-3xl font-bold">Hello world!</h1>
       <h2>{process.env.NODE_ENV}</h2>
       <h2>{process.env.CLIENT_ID}</h2>
       <LoginGoogle></LoginGoogle>
-    </div>
+    </>
   );
 }
 
